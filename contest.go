b935b3b4d6c3f62ce81841b3945f20d528945167
Mart Pluijmaekers <mart@tuupke.nl>
2021-04-12 23:15:14 +0200

    Refactored the code to more go-idiomatic style
    The structure of the code was left mostly intact. The http client has
    been altered to always include basic auth when a username and password
    has been set.
    
    Some basic error handling is also included now, though this will need to
    be extended.

--- contest.go
+++ contest.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // Current use:
@@ -16,137 +18,64 @@ import (
 //   contest -url <baseURL> -c contestId -u <user> -p <password> submit <text>
 // -insecure is optional, the rest isn't. Problem id is hardcoded to "checks" for now
 
-var baseURL string
-var contestId string
-var user string
-var password string
+type (
+	Contest struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	}
 
-type Contest struct {
-	Id   string
-	Name string
+	Problem struct {
+		Label string `json:"label"`
+		Name  string `json:"name"`
+	}
+
+	// Implmentation of the http.RoundTripper interface, used for always adding basic-auth
+	basicAuthTransport struct {
+		T http.RoundTripper
+	}
+)
+
+// RoundTrip adds the basic auth headers
+func (b basicAuthTransport) RoundTrip(request *http.Request) (*http.Response, error) {
+	if user != "" && password != "" {
+		request.SetBasicAuth(user, password)
+	}
+
+	return b.T.RoundTrip(request)
 }
 
-type Problem struct {
-	Label string
-	Name  string
-}
+var (
+	baseURL   string
+	contestId string
+	user      string
+	password  string
 
-func getJson(resp http.Response, target interface{}) error {
-	defer resp.Body.Close()
+	insecure bool
 
-	return json.NewDecoder(resp.Body).Decode(target)
-}
+	// Ensure BasicAuthTransport adheres to the interface
+	_ http.RoundTripper = basicAuthTransport{}
 
-func getContests(client http.Client) ([]Contest, error) {
-	req, err := http.NewRequest("GET", baseURL+"/contests/", nil)
-	if err != nil {
-		return nil, err
-	}
+	errUnauthorized = errors.New("request not authorized")
+	errNotFound     = errors.New("object not found")
+)
 
-	req.SetBasicAuth(user, password)
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	// parse response
-	var contests []Contest
-	err = getJson(*resp, &contests)
-	if err != nil {
-		return nil, err
-	}
-	return contests, nil
-}
-
-func getProblems(client http.Client) ([]Problem, error) {
-	req, err := http.NewRequest("GET", baseURL+"/contests/"+contestId+"/problems", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.SetBasicAuth(user, password)
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	// parse response
-	var problems []Problem
-	err = getJson(*resp, &problems)
-	if err != nil {
-		return nil, err
-	}
-	return problems, nil
-}
-
-func postClarification(client http.Client, problemId string, text string) (string, error) {
-	requestBody, err := json.Marshal(map[string]string{
-		"problem_id": problemId,
-		"text":       text,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	req, err := http.NewRequest("POST", baseURL+"/contests/"+contestId+"/clarifications", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return "", err
-	}
-
-	req.SetBasicAuth(user, password)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	// parse response
-	var clarificationId string
-	err = getJson(*resp, &clarificationId)
-	if err != nil {
-		return "", err
-	}
-	return clarificationId, nil
-}
-
-func postSubmission(client http.Client, problemId string, languageId string, file string) (string, error) {
-	requestBody, err := json.Marshal(map[string]string{
-		"problem_id":  problemId,
-		"language_id": languageId,
-		"file":        file,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	req, err := http.NewRequest("POST", baseURL+"/contests/"+contestId+"/submissions", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return "", err
-	}
-
-	req.SetBasicAuth(user, password)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	var submissionId string
-	err = getJson(*resp, &submissionId)
-	if err != nil {
-		return "", err
-	}
-	return submissionId, nil
-}
-
-func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
-	req.SetBasicAuth("team1", "team11")
-	return nil
-}
-
-func main() {
-	flag.StringVar(&baseURL, "url", "", "the base 'Contest URL'") // TODO if there is a trailing slash we should remove it
+func init() {
+	flag.StringVar(&baseURL, "url", "", "the base 'Contest URL'")
 	flag.StringVar(&contestId, "c", "", "the 'contest id'")
 	flag.StringVar(&user, "u", "", "the 'user'")
 	flag.StringVar(&password, "p", "", "the 'password'")
-	insecure := flag.Bool("insecure", false, "whether the connection is secure")
+	insecure = *flag.Bool("insecure", false, "whether the connection is secure")
 	flag.Parse()
+
+	// Strip trailing slash of the baseurl
+	baseURL = strings.TrimSuffix(baseURL, "/")
+}
+
+func main() {
+	if len(flag.Args()) == 0 {
+		fmt.Println("No command given")
+		return
+	}
 
 	if baseURL == "" {
 		fmt.Println("No base contest URL")
@@ -158,67 +87,154 @@ func main() {
 		return
 	}
 
-	if len(flag.Args()) == 0 {
-		fmt.Println("No command given")
-		return
-	}
-
-	if *insecure {
+	// Create a transport for insecure communication and adding of basic-auth headers
+	transport := http.DefaultTransport
+	if insecure {
 		fmt.Println("Insecure")
-		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	}
-	client := &http.Client{
-		//Jar: cookieJar,
-		CheckRedirect: redirectPolicyFunc,
+		transport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
-	if flag.Args()[0] == "clar" {
-		if len(flag.Args()) != 2 {
+	// Create client and transport for basic auth
+	client := http.Client{
+		Transport: basicAuthTransport{transport},
+	}
+
+	// flag.Arg always return empty string if arg is not set
+	var text string = flag.Arg(1)
+	var hasText = text != ""
+
+	// Switch is preferred over large else-if chains, though sometimes (as with this nested case) else-if is clearly more
+	// legible
+	switch flag.Arg(0) {
+	case "clar":
+		if !hasText {
 			fmt.Println("Clarification text not provided correctly")
-			return
-		}
-
-		text := flag.Args()[1]
-		clarificationId, err := postClarification(*client, "checks", text)
-		if err != nil {
+		} else if clarificationId, err := postClarification(client, "checks", text); err != nil {
 			fmt.Println("Error submitting clarification: ", err)
-			return
 		} else {
 			fmt.Println("Clarification submitted successfully! ", clarificationId)
 		}
-	} else if flag.Args()[0] == "submit" {
-		if len(flag.Args()) != 2 {
+	case "submit":
+		if !hasText {
 			fmt.Println("Submission not provided correctly")
-			return
-		}
-
-		text := flag.Args()[1]
-		submissionId, err := postSubmission(*client, text, text, text)
-		if err != nil {
-			fmt.Println("Error submitting: ", err)
-			return
+		} else if submissionId, err := postSubmission(client, text, text, text); err != nil {
+			fmt.Println("Error submitting:", err)
 		} else {
-			fmt.Println("Submitted successfully! ", submissionId)
+			fmt.Println("Submitted successfully!", submissionId)
 		}
-	} else if flag.Args()[0] == "contests" {
-		contests, err := getContests(*client)
-		if err != nil {
-			fmt.Println("Error getting contests: ", err)
-			return
+	case "contests":
+		if contests, err := getContests(client); err != nil {
+			fmt.Println("Error getting contests:", err)
 		} else {
-			fmt.Println("Contests found successfully! ", contests)
+			fmt.Println("Contests found successfully!", contests)
 			fmt.Printf("Contests: %+v", contests)
 		}
-	} else if flag.Args()[0] == "problems" {
-		problems, err := getProblems(*client)
-		if err != nil {
-			fmt.Println("Error getting problems: ", err)
-			return
+	case "problems":
+		if problems, err := getProblems(client); err != nil {
+			fmt.Println("Error getting problems:", err)
 		} else {
-			fmt.Println("Problems found successfully! ", problems)
+			fmt.Println("Problems found successfully!", problems)
 			fmt.Printf("Problems: %+v", problems)
 		}
-	} else {
-		fmt.Println("Only 'clar' command is supported")
+	default:
+		fmt.Println("Only 'clar', 'submit', 'contests', and 'problems' commands are supported")
+	}
+}
+
+func getContests(client http.Client) ([]Contest, error) {
+	var contests []Contest
+
+	resp, err := client.Get(baseURL + "/contests")
+	// Body is always non-nil, ensure it will always be closed
+	defer resp.Body.Close()
+	if err != nil {
+		return contests, err
+	} else if err := handleStatus(resp.StatusCode); err != nil {
+		return contests, err
+	}
+
+	// Parse response, no need for specific control flow on error
+	return contests, json.NewDecoder(resp.Body).Decode(&contests)
+}
+
+func getProblems(client http.Client) ([]Problem, error) {
+	var problems []Problem
+
+	resp, err := client.Get(baseURL + "/contests/" + contestId + "/problems")
+	// Body is always non-nil, ensure it will always be closed
+	defer resp.Body.Close()
+	if err != nil {
+		return problems, err
+	} else if err := handleStatus(resp.StatusCode); err != nil {
+		return problems, err
+	}
+
+	// Parse response, no need for specific control flow on error
+	return problems, json.NewDecoder(resp.Body).Decode(&problems)
+}
+
+func postClarification(client http.Client, problemId string, text string) (string, error) {
+	var clarificationId string
+
+	var buf = new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+
+	// Perhaps replace this with a struct?
+	err := enc.Encode(map[string]string{
+		"problem_id": problemId,
+		"text":       text,
+	})
+
+	if err != nil {
+		return clarificationId, err
+	}
+
+	resp, err := client.Post(baseURL+"/contests/"+contestId+"/clarifications", "application/json", buf)
+	defer resp.Body.Close()
+	if err != nil {
+		return clarificationId, err
+	} else if err := handleStatus(resp.StatusCode); err != nil {
+		return clarificationId, err
+	}
+
+	// parse response
+	return clarificationId, json.NewDecoder(resp.Body).Decode(&clarificationId)
+}
+
+func postSubmission(client http.Client, problemId string, languageId string, file string) (string, error) {
+	var submissionId string
+
+	var buf = new(bytes.Buffer)
+	err := json.NewEncoder(buf).Encode(map[string]string{
+		"problem_id":  problemId,
+		"language_id": languageId,
+		"file":        file,
+	})
+
+	if err != nil {
+		return submissionId, err
+	}
+
+	resp, err := client.Post(baseURL+"/contests/"+contestId+"/submissions", "application/json", buf)
+	if err != nil {
+		return submissionId, err
+	} else if err := handleStatus(resp.StatusCode); err != nil {
+		return "", err
+	}
+
+	// parse response
+	return submissionId, json.NewDecoder(resp.Body).Decode(&submissionId)
+}
+
+func handleStatus(status int) error {
+	switch status {
+	case http.StatusOK:
+		return nil
+	case http.StatusUnauthorized:
+		return errUnauthorized
+	case http.StatusNotFound:
+		return errNotFound
+	default:
+		return fmt.Errorf("invalid statuscode received: %d", status)
 	}
 }
