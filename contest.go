c9d75d5bbc82c2d44b86444b46f60c6bf9b279fd
Nicky Gerritsen <nickygerritsen@me.com>
2021-04-26 12:13:59 +0200

    Remove unneeded variables from main package.

--- contest.go
+++ contest.go
@@ -1,36 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"tools.icpc.global/contest/commands"
 )
-
-// Current use:
-//   contest -url <baseURL> -u <user> -p <password> contests
-//   contest -url <baseURL> -c contestId -u <user> -p <password> problems
-//   contest -url <baseURL> -c contestId -u <user> -p <password> clar <text>
-//   contest -url <baseURL> -c contestId -u <user> -p <password> submit <text>
-// -insecure is optional, the rest isn't. Problem id is hardcoded to "checks" for now
-
-var (
-	baseURL   string
-	contestId string
-	user      string
-	password  string
-
-	insecure bool
-)
-
-func init() {
-	flag.StringVar(&baseURL, "url", "", "the base 'Contest URL'")
-	flag.StringVar(&contestId, "c", "", "the 'contest id'")
-	flag.StringVar(&user, "u", "", "the 'user'")
-	flag.StringVar(&password, "p", "", "the 'password'")
-	flag.BoolVar(&insecure, "insecure", false, "whether the connection is secure")
-}
 
 func main() {
 	err := commands.Execute()
