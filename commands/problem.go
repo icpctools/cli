f02f56cf08a88397babcc89535fc1a3d54170626
Tim deBoer <tim@tdeboer.ca>
2021-04-28 16:22:10 -0400

    Ordering and simpler output for problems

--- commands/problem.go
+++ commands/problem.go
@@ -3,9 +3,10 @@ package commands
 import (
 	"errors"
 	"fmt"
-	"github.com/spf13/viper"
+	"sort"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	interactor "github.com/tuupke/api-interactor"
 )
 
@@ -34,6 +35,16 @@ func fetchProblems(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("could not retrieve problems; %w", err)
 	}
 
-	_, err = fmt.Fprint(cmd.OutOrStdout(), p)
-	return err
+	// sort by ordinal
+	sort.Slice(p, func(i, j int) bool {
+		return p[i].Ordinal < p[j].Ordinal
+	})
+
+	// output
+	fmt.Printf("Problems (%d):\n", len(p))
+	for _, o := range p {
+		fmt.Printf(" %3s: %s\n", o.Label, o.Name)
+	}
+
+	return nil
 }
