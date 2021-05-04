6ef605c4010cb77f4ebe3b5148a5dd64e1110a2e
Tim deBoer <git@tdeboer.ca>
2021-05-03 21:18:20 -0400

    Listing clarifications
    This could use more work and maybe there's a better way to output clarifications, but for now it adds the initial command ("clar" for now) to list all of the team's clarifications. Each clarification is output with a message to say whether the team submitted it, it's a response, or a broadcast. The contest time is also shown, if it was associated with a problem, and the text, e.g.:
    
    Clarifications (2):
      Clarification sent to jury at 1h38m58.21s for problem E (Dead-End Detector):
         In case n = 2 and m = 1, there are two solutions:
         1 2
         and
         2 1
    
         Is it allowed to print any of them?
      Response from jury at 1h39m54.235s for problem E (Dead-End Detector):
         > In case n = 2 and m = 1, there are two solutions:
         > 1 2
         > and
         > 2 1
         >
         > Is it allowed to print any of them?
    
         No comment

---
+++ commands/clar.go
@@ -0,0 +1,60 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var clarCommand = &cobra.Command{
+	Use:     "clar",
+	Short:   "Get clarifications",
+	RunE:    fetchClars,
+	PreRunE: configHelper("baseurl", "contest"),
+}
+
+func fetchClars(cmd *cobra.Command, args []string) error {
+	api, err := contestApi()
+	if err != nil {
+		return fmt.Errorf("could not connect to the API; %w", err)
+	}
+
+	clars, err := api.Clarifications()
+
+	if err != nil {
+		return fmt.Errorf("could not retrieve clarifications; %w", err)
+	}
+
+	// output
+	fmt.Printf("Clarifications (%d):\n", len(clars))
+	for _, o := range clars {
+		if o.FromTeamId == "" && o.ToTeamId == "" {
+			fmt.Printf("  Broadcast message from jury at %v", o.ContestTime)
+		} else if o.FromTeamId != "" {
+			fmt.Printf("  Clarification sent to jury at %v", o.ContestTime)
+		} else {
+			fmt.Printf("  Response from jury at %v", o.ContestTime)
+		}
+		if o.ProblemId != "" {
+			problems, err := api.Problems()
+			if err != nil {
+				return fmt.Errorf("could not get problems; %w", err)
+			}
+
+			problem, hasProblem := problemSet(problems).byId(o.ProblemId)
+			if !hasProblem {
+				fmt.Printf(" for unknown problem")
+			}
+
+			fmt.Printf(" for problem %s (%s)", problem.Label, problem.Name)
+		}
+		fmt.Printf(":\n")
+		lines := strings.Split(o.Text, "\n")
+		for _, s := range lines {
+			fmt.Printf("     %s\n", s)
+		}
+	}
+
+	return nil
+}
