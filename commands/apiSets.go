332bd18be291cccdaafbc6089b00eec0f30604fa
Tim deBoer <git@tdeboer.ca>
2021-05-10 09:04:21 -0400

    Tables & API sets
    Moving two utilities that are each used in multiple places to their own files: tables and the API sets

---
+++ commands/apiSets.go
@@ -0,0 +1,78 @@
+package commands
+
+import (
+	"strings"
+
+	interactor "github.com/tuupke/api-interactor"
+)
+
+type (
+	judgementTypeSet []interactor.JudgementType
+	problemSet       []interactor.Problem
+	languageSet      []interactor.Language
+	judgementSet     []interactor.Judgement
+	teamSet          []interactor.Team
+)
+
+func (j judgementTypeSet) byId(id string) (interactor.JudgementType, bool) {
+	for _, jt := range j {
+		if jt.Id == id {
+			return jt, true
+		}
+	}
+
+	return interactor.JudgementType{}, false
+}
+
+func (t teamSet) byId(id string) (interactor.Team, bool) {
+	for _, team := range t {
+		if team.Id == id {
+			return team, true
+		}
+	}
+
+	return interactor.Team{}, false
+}
+
+func (j judgementSet) bySubmissionId(id string) (judgementSet, bool) {
+	var jud []interactor.Judgement
+	for _, ju := range j {
+		if ju.SubmissionId == id {
+			jud = append(jud, ju)
+		}
+	}
+
+	return jud, len(jud) > 0
+}
+
+func (p problemSet) byId(id string) (interactor.Problem, bool) {
+	for _, problem := range p {
+		if strings.EqualFold(problem.Id, id) || strings.EqualFold(problem.Label, id) || strings.EqualFold(problem.Name, id) {
+			return problem, true
+		}
+	}
+
+	return interactor.Problem{}, false
+}
+
+func (l languageSet) byId(id string) (interactor.Language, bool) {
+	for _, language := range l {
+		if language.Id == id {
+			return language, true
+		}
+	}
+
+	return interactor.Language{}, false
+}
+
+func (l languageSet) byExtension(extension string) (interactor.Language, bool) {
+	for _, language := range l {
+		for _, languageExtension := range language.Extensions {
+			if strings.ToLower(languageExtension) == extension {
+				return language, true
+			}
+		}
+	}
+
+	return interactor.Language{}, false
+}
--- commands/scoreboard.go
+++ commands/scoreboard.go
@@ -6,76 +6,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type rowStr []string
-
-const ALIGN_LEFT = 0
-const ALIGN_RIGHT = 1
-
-type Table struct {
-	Header rowStr
-	Rows   []rowStr
-	Align  []int
-}
-
 var scoreboardCommand = &cobra.Command{
 	Use:     "scoreboard",
 	Short:   "Show the contest scoreboard",
 	RunE:    scoreboard,
 	PreRunE: configHelper("baseurl"),
-}
-
-func (table *Table) appendRow(row []string) {
-	table.Rows = append(table.Rows, row)
-}
-
-func (table Table) print() error {
-	// determine the amount of padding needed
-	var numCol = len(table.Header)
-	var maxLength []int = make([]int, numCol)
-	var format []string = make([]string, numCol)
-
-	// find max header width
-	for i, s := range table.Header {
-		if maxLength[i] < len(s) {
-			maxLength[i] = len(s)
-		}
-	}
-
-	// find max cell width
-	for _, r := range table.Rows {
-		for i, s := range r {
-			if maxLength[i] < len(s) {
-				maxLength[i] = len(s)
-			}
-		}
-	}
-
-	// create format for each column, respecting width and alignment
-	for i := range table.Header {
-		if table.Align[i] == ALIGN_LEFT {
-			format[i] = fmt.Sprintf(" %%-%vv ", maxLength[i])
-		} else {
-			format[i] = fmt.Sprintf(" %%%vv ", maxLength[i])
-		}
-	}
-
-	// output header bold and underlined
-	fmt.Printf("  \033[1;4m")
-	for i, k := range table.Header {
-		fmt.Printf(format[i], k)
-	}
-	fmt.Printf("\033[0m\n")
-
-	// output each cell
-	for _, r := range table.Rows {
-		fmt.Printf("  ")
-		for i, s := range r {
-			fmt.Printf(format[i], s)
-		}
-		fmt.Printf("\n")
-	}
-
-	return nil
 }
 
 func scoreboard(cmd *cobra.Command, args []string) error {
